9d5fd306104f2cf50a8a34b27ad941f2ee462760
productivejamesw <james.walters@productive.com>
2016-10-24 21:34:05 -0500

    publish new version of setf in set_functions

--- R/set_functions.R
+++ R/set_functions.R
@@ -1,12 +1,11 @@
 #' @title set functions for multiple column alterations using data.tables
 #'
-#' @description This convenience function updates by reference multiple columns
-#'  by applying a desired function such as \code{numeric}, \code{factor} using
-#'  functions in the same manor as \code{apply()}, or with \code{.} as a
-#'  placeholder. The purpose is to avoid a for loop in conjunction with
-#'  data.table::set.
+#' @description This function updates by reference multiple columns by applying
+#'   a desired function such as \code{numeric}, \code{factor} using functions in
+#'   the same manner as \code{apply()}, or with \code{.} as a placeholder.
 #'
-#' @param x A data.table. Or, set() accepts data.frame, too.
+#' @param x A data.table. Or, set() accepts data.frame, too. If you are
+#' creating new columns, x should be a data.table.
 #'
 #' \code{i} Is left out of this function.
 #'
@@ -16,40 +15,35 @@
 #'   reference all columns in the data.table.
 #' @param value  A list of replacement values to assign by reference to
 #'   \code{x[i, j]}. In addition, functions can be passed into the value
-#'   argument as well with subsquent arguments in the same way as apply
+#'   argument as well with subsquent arguements in the same way as apply
 #'   functions.
 #' @param ... Is used to pass arguments into the value "function" if you choose
 #'   to apply a function instead of updating the data with a replacement vector
 #'   or list. If you choose to place a \code{.} at any point within the
-#'   sequence of additional arguments passed into the \code{value} argument,
+#'   sequence of additional arguements passed into the \code{value} argument,
 #'   the \code{.} will act as a placeholder for the jth column.
 #'
-#' @section Notes: These functions are for convenience only.
+#' @note These functions are for convenience only.
+#'
+#' @importFrom data.table set
+#' @examples
+#' dt <- d.t(x = 0:10, y = 10:20)
+#' setf(dt, j = c("x2","y"), value = paste0,  ., "hello", "would")
+#' print(dt)
+#' df2 <- d.f(x = 0:10, y = 10:20)
+#' setf(df2, j = c("x","y"), value = paste0,  ., "hello", "would")
+#' print(df2)
 #'
 #' @name set_functions
 NULL
 
 #' @rdname set_functions
-#' @importFrom data.table set
-#' @examples
-#' # Data Table Example 1
-#' dt <- d.t(x = 0:10, y = 10:20)
-#' setf(dt, j = c("x","y"), value = paste0,  ., "hello", "world")
-#' print(dt)
-#'
-#' # Data Table Example 2 (same result as Data Table Example 1)
-#' dt <- d.t(x = 0:10, y = 10:20)
-#' setf(dt, j = c("x","y"),
-#'  value = function(x) paste0(x, "hello", "world"))
-#' print(dt)
-#'
-#' # Data Frame Example
-#' df_test <- data.frame(a=1:10, b = 2:11, c = 3:12)
-#' setf(df_test, j = c("a", "c"), value = function(x) x+4)
-#' print(df_test)
-#'
 #' @export
-setf <- function(x, j, value, ...) {
+setf <- function (x, j, value, ...) {
+  if (!any(class(x) == "data.frame")) {
+    stop("x must be class data.table or data.frame, not a ",
+         paste(class(x), sep = "", collapse = ", "))
+  }
   if (!'all' %in% names(x) && j == "all") j <- names(x)
   if (is.atomic(value)) {
     set(x, i = NULL, j, value)
@@ -63,10 +57,8 @@ setf <- function(x, j, value, ...) {
       clist <- append(list(value), args[-ph_index])
     }
     FUN <- as.call(append(clist, quote(x[[jj]]), after = ph_index))
-    for (jj in j) {
-      eval(substitute(set(x, i = NULL, j = jj, value = f), list(f = FUN)))
+    for(jj in j) {
+      eval(substitute(set(x, i=NULL, j=jj, value = f), list(f = FUN)))
     }
   }
 }
-
-
